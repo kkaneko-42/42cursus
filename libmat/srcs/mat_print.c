dbbb1f4df7c4c154412f4abdd54fd16c8e8f16fa
kkaneko <kkaneko@student.42tokyo.jp>
2022-03-14 23:45:17 +0900

    create mat_print.c

---
+++ libmat/srcs/mat_print.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mat_print.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkaneko <kkaneko@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/14 23:35:06 by kkaneko           #+#    #+#             */
+/*   Updated: 2022/03/14 23:44:56 by kkaneko          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libmat.h"
+#include <stdio.h>
+
+void	mat_print(t_matrix *mat)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	while (i < mat->row)
+	{
+		j = 0;
+		while (j < mat->col)
+		{
+			printf("%ld ", mat->values[i][j]);
+			++j;
+		}
+		printf("\n");
+		++i;
+	}
+}
