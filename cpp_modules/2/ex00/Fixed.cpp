c195aca8b1420495becf0d9a067425506779718e
kkaneko <kkaneko@student.42tokyo.jp>
2022-03-21 22:37:12 +0900

    cpp2-ex00 functionable (there is implementation in header)

--- cpp_modules/2/ex00/Fixed.cpp
+++ cpp_modules/2/ex00/Fixed.cpp
@@ -1,2 +1,38 @@
 #include <iostream>
 #include <string>
+#include "Fixed.hpp"
+
+Fixed::Fixed( void ) : raw_bits_(0)
+{
+	const std::string msg = "Default constructor called";
+
+	std::cout << msg << std::endl;
+}
+
+Fixed::Fixed( const Fixed &src )
+{
+	const std::string msg = "Copy constructor called";
+
+	std::cout << msg << std::endl;
+	*this = src;
+}
+
+Fixed::~Fixed( void )
+{
+	const std::string msg = "Destructor called";
+
+	std::cout << msg << std::endl;
+}
+
+int Fixed::getRawBits( void ) const
+{
+	const std::string msg = "getRawBits member function called";
+
+	std::cout << msg << std::endl;
+	return (this->raw_bits_);
+}
+
+void Fixed::setRawBits( int const raw )
+{
+	this->raw_bits_ = raw;
+}
--- cpp_modules/2/ex00/Fixed.hpp
+++ cpp_modules/2/ex00/Fixed.hpp
@@ -1,7 +1,27 @@
 #ifndef FIXED_HPP
 # define FIXED_HPP
 
+class Fixed
+{
+	public:
+		Fixed( void );
+		Fixed( const Fixed &src );
+		~Fixed( void );
+		Fixed &operator =( const Fixed &src )
+		{
+			const std::string msg = "Assignation operator called";
 
+			std::cout << msg << std::endl;
+			this->setRawBits(src.raw_bits_);
+			return (*this);
+		}
 
+		int getRawBits( void ) const;
+		void setRawBits( int const raw );
+
+	private:
+		int raw_bits_;
+		static const int nb_fract_bits_ = 8;
+};
 
 #endif //FIXED_HPP
