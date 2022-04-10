71aa1d9db37d297bc378cf4651bff7afb218334c
kkaneko <kkaneko@student.42tokyo.jp>
2022-04-10 16:15:10 +0900

    cpp4-ex03 functionable

--- cpp_modules/4/ex02/src/main.cpp
+++ cpp_modules/4/ex02/src/main.cpp
@@ -2,11 +2,13 @@
 #include "Animal.hpp"
 #include "Dog.hpp"
 #include "Cat.hpp"
+#include "AAnimal.hpp"
 
 void DefaultTest( void )
 {
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
+	// const AAnimal* k = new AAnimal();
 
 	delete j;//should not create a leak
 	delete i;
