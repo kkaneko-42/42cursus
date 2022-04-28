01f415e66dce8b0c1779e94a2e6a857f0a881298
Kazuhiko Kaneko <kkaneko@c1r20s7.42tokyo.jp>
2022-04-28 18:52:04 +0900

    go-1 ex07 functionable

--- go_piscine/1/ex07/vendor/piscine/strrev.go
+++ go_piscine/1/ex07/vendor/piscine/strrev.go
@@ -1,7 +1,7 @@
 package piscine
 
 func StrLen(s string) int {
-	var len int = 0
+	var len int = 0;
 
 	for range s {
 		len++
@@ -9,11 +9,21 @@ func StrLen(s string) int {
 	return len
 }
 
+func SwapRune(a *rune, b *rune) {
+	var tmp rune
+
+	tmp = *a
+	*a = *b
+	*b = tmp
+}
+
 func StrRev(s string) string {
-	var res []rune
+	res := []rune(s)
 	s_len := StrLen(s)
 
-	for i, c range s {
-		res[i]
+	for i := 0; i < s_len / 2; i++{
+		SwapRune(&res[i], &res[s_len - i - 1])
 	}
+
+	return (string(res))
 }
